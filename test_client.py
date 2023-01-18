05ac201f5d66adee1cd26154b082490dec5aaf03
ea7kir <mikenaylorspain@icloud.com>
2023-01-19 00:57:14 +0100

    tidy

--- test_client.py
+++ test_client.py
@@ -11,14 +11,15 @@ async def listen():
     # Connect to the server
     async with websockets.connect(URL) as ws:
         # Send a greeting message
-        await ws.send("Hello Server!")
+        #await ws.send("Hello Server!")
         # Stay alive forever, listening to incoming msgs
         while True:
             msg = await ws.recv()
             print(msg)
 
 # Start the connection
-#asyncio.get_event_loop().run_until_complete(listen())
-asyncio.run(listen)
+asyncio.get_event_loop().run_until_complete(listen())
+asyncio.get_event_loop().run_forever()
+#asyncio.run(listen)
 
 # can also test $ python -m websockets ws://roof.local:8765/
