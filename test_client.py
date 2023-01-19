a152a01dfe7a2f4e9d8b27c66a8377ef60435399
ea7kir <mikenaylorspain@icloud.com>
2023-01-19 13:03:50 +0100

    allmost

--- test_client.py
+++ test_client.py
@@ -16,19 +16,23 @@ roof_data = RoofData()
 def process_read_roof_data(connection):
     async def handle():
         url = f'ws://{SERVER}:{PORT}/'
-        async with websockets.connect(url) as websocket:
-            print('connected', flush=True)
-            roof_data.connected = True
-            # NOT REQUIRED - roof_data = RoofData()
-            while True:
-                # if message to send
-                has_message = True
-                if has_message:
-                    message = 'PTT'
-                    await websocket.send(message)
-                
-                roof_data.counter = await websocket.recv() # TODO: JSON
-                print(roof_data.counter, flush=True)
+        try:
+            async with websockets.connect(url) as websocket:
+                print('connected', flush=True)
+                roof_data.connected = True
+                # NOT REQUIRED - roof_data = RoofData()
+                while True:
+                    # if message to send
+                    has_message = True
+                    if has_message:
+                        message = 'PTT'
+                        await websocket.send(message)
+
+                    roof_data.counter = await websocket.recv() # TODO: JSON
+                    print(roof_data.counter, flush=True)
+        except: 
+            print('not connected', flush=True)
+            roof_data.connected = False
 
     asyncio.run(handle())
 
