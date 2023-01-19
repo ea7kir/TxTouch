0547fca84995e69db1dc166035a15b6885c103bc
ea7kir <mikenaylorspain@icloud.com>
2023-01-19 11:45:58 +0100

    allmost

--- test_client.py
+++ test_client.py
@@ -25,20 +25,28 @@ URL = f'ws://{SERVER}:{PORT}/'
 
 # can also test $ python -m websockets ws://roof.local:8765/
 
+class RoofData:
+    counter = 0
+    connected = False
+
+roof_data = RoofData()
+
 def process_read_roof_data(connection):
     async def handle():
         url = f'ws://{SERVER}:{PORT}/'
         async with websockets.connect(url) as websocket:
+            print('connected', flush=True)
+            roof_data.connected = True
             # NOT REQUIRED - roof_data = RoofData()
             while True:
                 # if message to send
                 has_message = True
                 if has_message:
-                    message = 'Hi'
+                    message = 'PTT'
                     await websocket.send(message)
                 
-                roof_data = await websocket.recv()
-                print(roof_data)
+                roof_data.counter = await websocket.recv() # TODO: JSON
+                print(roof_data.counter, flush=True)
 
     asyncio.run(handle())
 
