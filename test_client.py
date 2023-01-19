50dd9ba0b835b30efe9e03b39ef9bf737a1bbb7a
ea7kir <mikenaylorspain@icloud.com>
2023-01-19 11:46:59 +0100

    allmost

--- test_client.py
+++ test_client.py
@@ -5,24 +5,6 @@ SERVER = 'roof.local'
 PORT = 8765
 URL = f'ws://{SERVER}:{PORT}/'
 
-# The main function that will handle connection and communication 
-# with the server
-
-# async def listen():
-#     # Connect to the server
-#     async with websockets.connect(URL) as ws:
-#         # Send a greeting message
-#         #await ws.send("Hello Server!")
-#         # Stay alive forever, listening to incoming msgs
-#         while True:
-#             msg = await ws.recv()
-#             print(msg)
-# 
-# # Start the connection
-# asyncio.get_event_loop().run_until_complete(listen())
-# #asyncio.get_event_loop().run_forever()
-
-
 # can also test $ python -m websockets ws://roof.local:8765/
 
 class RoofData:
