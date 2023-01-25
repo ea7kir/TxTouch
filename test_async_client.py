095d2a2b35c9bdac3bb694c8f5571ccc306e7041
ea7kir <mikenaylorspain@icloud.com>
2023-01-25 10:46:23 +0100

    test_async working

--- test_async_client.py
+++ test_async_client.py
@@ -5,10 +5,10 @@ SERVER = 'roof.local'
 PORT = 8765
 
 class RoofData:
-    preamp_temp:str = ''
-    pa_temp: str = ''
-    pa_current: str = ''
-    fans: str = ''
+    preamp_temp:str = '-'
+    pa_temp: str = '-'
+    pa_current: str = '-'
+    fans: str = '-'
     connected: bool = False
 
 def consume(roof_data):
@@ -21,23 +21,32 @@ def consume(roof_data):
 
 # developed from "TCP echo server using streams"
 # https://docs.python.org/3/library/asyncio-stream.html#tcp-echo-client-using-streams
-async def client():
-    reader, writer = await asyncio.open_connection(SERVER, PORT)
-    print('CONNECTED TO SERVER', flush=True)
-    roof_data = RoofData()
-    while True:
-        data_in = await reader.read(1024)
-        data_str = data_in.decode()
-        data = json.loads(data_str)
-        roof_data.preamp_temp = data['preamp_temp']
-        roof_data.pa_temp = data['pa_temp']
-        roof_data.pa_current = data['pa_current']
-        roof_data.fans = data['fans']
-        roof_data.connected = data['connected']
-        consume(roof_data)
 
-    print('Close the connection', flush=True)
-    writer.close()
+async def client():
+    try:
+        reader, writer = await asyncio.open_connection(SERVER, PORT)
+        print('CONNECTED TO SERVER', flush=True)
+        roof_data = RoofData()
+        while True:
+            try:
+                data_in = await reader.read(1024)
+                data_str = data_in.decode()
+                data = json.loads(data_str)
+                roof_data.preamp_temp = data['preamp_temp']
+                roof_data.pa_temp = data['pa_temp']
+                roof_data.pa_current = data['pa_current']
+                roof_data.fans = data['fans']
+                roof_data.connected = data['connected']
+                consume(roof_data)
+            except:
+                break
+        print('Close the connection', flush=True)
+        writer.close()
+    except:
+        print('Server is unavaliable', flush=True)
+
+    roof_data = RoofData()
+    consume(roof_data)
 
 asyncio.run(client())
 
