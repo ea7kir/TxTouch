c16b2141030b2a19be9e27d8da77b488fed1ddd8
ea7kir <mikenaylorspain@icloud.com>
2023-01-26 00:27:44 +0100

    tidy

--- test_async_client.py
+++ test_async_client.py
@@ -9,14 +9,14 @@ class RoofData:
     pa_temp: str = '-'
     pa_current: str = '-'
     fans: str = '-'
-    connected: bool = False
+    #connected: bool = False
 
-def consume(roof_data):
+async def consume(roof_data):
     print(f'roof_data.preamp_temp : {roof_data.preamp_temp}', flush=True)
     print(f'roof_data.pa_temp     : {roof_data.pa_temp}', flush=True)
     print(f'roof_data.pa_current  : {roof_data.pa_current}', flush=True)
     print(f'roof_data.fans        : {roof_data.fans}', flush=True)
-    print(f'roof_data.connected   : {roof_data.connected}', flush=True)
+    #print(f'roof_data.connected   : {roof_data.connected}', flush=True)
     print('-------------------------------------', flush=True)
 
 # developed from "TCP echo server using streams"
@@ -25,25 +25,35 @@ def consume(roof_data):
 async def client():
     try:
         reader, writer = await asyncio.open_connection(SERVER, PORT)
-        print('CONNECTED TO SERVER', flush=True)
-        roof_data = RoofData()
-        while True:
-            try:
-                data_in = await reader.read(1024)
-                data_str = data_in.decode()
-                data = json.loads(data_str)
-                roof_data.preamp_temp = data['preamp_temp']
-                roof_data.pa_temp = data['pa_temp']
-                roof_data.pa_current = data['pa_current']
-                roof_data.fans = data['fans']
-                roof_data.connected = data['connected']
-                consume(roof_data)
-            except:
+        print('Connected to server', flush=True)
+    except IOError as e:
+        print(f'EXCEPTION 1 {e}', flush=True)
+        return
+        if e != 'connected':
+            print('Server is unavaliable', flush=True)
+            print(f'EXCEPTION 1 {e}', flush=True)
+            return    
+
+    roof_data = RoofData()
+    while True:
+        try:
+            print('waitin for more', flush=True)
+            json_dict_raw = await reader.read(1024)
+            json_dict = json_dict_raw.decode()
+            print(f'{json_dict}', flush=True)
+            data_dict = json.loads(json_dict)
+            roof_data.preamp_temp = data_dict['preamp_temp']
+            roof_data.pa_temp = data_dict['pa_temp']
+            roof_data.pa_current = data_dict['pa_current']
+            roof_data.fans = data_dict['fans']
+            #roof_data.connected = data_dict['connected']
+            await consume(roof_data)
+        except IOError as e:
+            print(f'EXCEPTION 2 {e}', flush=True)
+            break
+            if e != 'connected':
+                print(f'EXCEPTION 2 {e}', flush=True)
                 break
-        print('Close the connection', flush=True)
-        writer.close()
-    except:
-        print('Server is unavaliable', flush=True)
 
     roof_data = RoofData()
     consume(roof_data)
