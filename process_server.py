e9e49b6cfbfd8c57c008f373794a3ce28f2461c1
ea7kir <mikenaylorspain@icloud.com>
2023-01-27 13:57:34 +0100

    230127.1357

--- process_server.py
+++ process_server.py
@@ -1,4 +1,5 @@
-from time import sleep # ONLY NEEDED TO SIMULATE FETCH TIMES DURING DEVELOPMENT
+import asyncio
+import json
 
 SERVER = 'txserver.local'
 PORT = 8765
@@ -10,14 +11,33 @@ class ServerData:
     fans = '-'
 
 def process_read_server_data(pipe):
-    connected = False
-    server_data = ServerData()
-    while True:
-        if connected:
-            pass # async client goes here
-        else:
-            pipe.send(server_data)
-            sleep(1)
-  
+    # developed from "TCP echo client using streams"
+    # https://docs.python.org/3/library/asyncio-stream.html#tcp-echo-client-using-streams
+
+    async def client():
+        try:
+            reader, writer = await asyncio.open_connection(SERVER, PORT)
+            print(f'Connected to {SERVER}:{PORT}', flush=True)
+        except:
+            print(f'Failed to connected to {SERVER}:{PORT}', flush=True)
+
+        server_data = ServerData()
+        while True:
+            try:
+                json_dict_raw = await reader.read(1024)
+                json_dict = json_dict_raw.decode()
+                data_dict = json.loads(json_dict)
+                server_data.preamp_temp = data_dict['preamp_temp']
+                server_data.pa_temp = data_dict['pa_temp']
+                server_data.pa_current = data_dict['pa_current']
+                server_data.fans = data_dict['fans']
+                pipe.send(server_data)
+            except:
+                print(f'Connection to {SERVER}:{PORT} Failed', flush=True)
+                break
 
 
+        server_data = ServerData()
+        pipe.send(server_data)
+
+    asyncio.run(client())
