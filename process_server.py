b6933cd949837f258593515c1e1c149754bf1cf8
ea7kir <mikenaylorspain@icloud.com>
2023-01-27 11:31:17 +0100

    230127.1131

--- process_server.py
+++ process_server.py
@@ -4,29 +4,18 @@ SERVER = 'txserver.local'
 PORT = 8765
 
 class ServerData:
-    preamp_temp:str = ''
-    pa_temp: str = ''
-    pa_current: str = ''
-    fans: str = ''
+    preamp_temp = '-'
+    pa_temp = '-'
+    pa_current = '-'
+    fans = '-'
 
 def process_read_server_data(pipe):
-
+    connected = False
     server_data = ServerData()
-
     while True:
-
-        connected = False # temporary fix til I implement the connection
-
         if connected:
-
             pass # async client goes here
-
         else:
-
-            server_data.preamp_temp = '-'
-            server_data.pa_temp:str = '-'
-            server_data.pa_current:str = '-'
-            server_data.fans:str = '-'
             pipe.send(server_data)
             sleep(1)
   
